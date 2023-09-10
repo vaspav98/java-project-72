f6f27f31e0d29fe3a86d0171e0ae3a7aa1f68518
Pavel Vasiliev <vas-pav@mail.ru>
2023-09-10 13:26:57 +0300

    fix schema.sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id          bigint PRIMARY KEY AUTO_INCREMENT,
+   -- id          bigint PRIMARY KEY AUTO_INCREMENT,
+    id          bigint GENERATED ALWAYS AS IDENTITY,
     name        varchar(255),
     created_at  timestamp
 );
