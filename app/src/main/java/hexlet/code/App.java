264f40abe472dbcad1bc61af902460822783dc2b
Pavel Vasiliev <vas-pav@mail.ru>
2023-09-19 21:08:10 +0300

    fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,7 +14,6 @@ import io.javalin.rendering.template.JavalinJte;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
